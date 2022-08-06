155d11768c3660053a6fb3065942f2a5c2e480bb
Jaswant Singh <jaswant8907@gmail.com>
2022-08-06 22:20:01 +0530

    initial commit

---
+++ passport.js
@@ -0,0 +1,26 @@
+const GithubStrategy = require("passport-github2").Strategy;
+const passport = require("passport");
+
+GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+
+passport.use(
+  new GithubStrategy(
+    {
+      clientID: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET,
+      callbackURL: "/auth/github/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      done(null, profile);
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
